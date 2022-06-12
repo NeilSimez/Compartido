8b93a7a3986d18891ad2ad3e26d6fe9fc64f7388
Kuro Denoir <neilsimez@gmail.com>
2022-06-11 22:05:13 -0500

    tercera subida

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.querySelector(".first").addEventListener('click', function(){
+    Swal.fire({
+        title:'Felicidades',
+        text:'Su cita se agendó correctamente',
+        icon:'success'
+    });
+  });
+  
+
+ 
